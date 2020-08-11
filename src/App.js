1f0dd53eeff0d9429c1a6c41cf30549d88b15d31
Kordian <k.kuczniak@gmail.com>
2020-08-11 15:13:58 +0200

    master

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './component/dashboard/Dashboard';
 import ProjectDetails from './component/projects/ProjectDetails'
 import SignIn from './component/auth/SignIn'
 import SignUp from './component/auth/SignUp'
-
+import CreateProject from './component/projects/CreateProject'
 
 class App extends Component {
   render(){
@@ -18,6 +18,7 @@ class App extends Component {
         <Route path='/project/:id' component={ProjectDetails} />
         <Route path='/signin' component={SignIn} />
         <Route path='/signup' component={SignUp} />
+        <Route path='/create' component={CreateProject} />
       </Switch>
     </div>
     </BrowserRouter>
